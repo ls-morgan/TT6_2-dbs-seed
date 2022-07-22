d459765fe2dd530353d3cea9cd41e48488e661ed
Chua Ming Siong <chuamingsiong@gmail.com>
2022-07-22 13:21:22 +0800

    empty commit last commit, this is the actual files

---
+++ src/components/exchangeColumns.js
@@ -0,0 +1,10 @@
+export const ExchangeRateColumns = [
+    {
+        Header: 'Currency',
+        accessor: 'exchange_currency'
+    },
+    {
+        Header: 'Exchange Rates',
+        accessor: 'rates'
+    }
+]
